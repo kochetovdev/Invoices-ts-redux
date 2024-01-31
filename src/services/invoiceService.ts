f59f65fd26a2efc47c5b921c7275165ab6b0ede2
Kochetov <kochetov.yevhen.dev@gmail.com>
2024-01-31 21:31:54 +0200

    add try caftch block to the invoiceService

--- src/services/invoiceService.ts
+++ src/services/invoiceService.ts
@@ -9,21 +9,37 @@ export class invoiceService {
   private static basePath = Route.BASE_PATH;
 
   static async getInvoices() {
-    return instance.get(invoiceService.basePath);
+    try {
+      return instance.get(invoiceService.basePath);
+    } catch (error) {
+      throw error;
+    }
   }
 
   static async addNewInvoise(newInvoice: Omit<Invoice, "date_supplied">) {
-    return await instance.post(invoiceService.basePath, newInvoice);
+    try {
+      return await instance.post(invoiceService.basePath, newInvoice);
+    } catch (error) {
+      throw error;
+    }
   }
 
   static async deleteInvoice(id: string) {
-    return await instance.delete(`${invoiceService.basePath}/${id}`);
+    try {
+      return await instance.delete(`${invoiceService.basePath}/${id}`);
+    } catch (error) {
+      throw error;
+    }
   }
 
   static async updateInvoice(updateInvoice: Omit<Invoice, "date_created">) {
-    return await instance.patch(
-      `${invoiceService.basePath}/${updateInvoice.id}`,
-      updateInvoice
-    );
+    try {
+      return await instance.patch(
+        `${invoiceService.basePath}/${updateInvoice.id}`,
+        updateInvoice
+      );
+    } catch (error) {
+      throw error;
+    }
   }
 }
